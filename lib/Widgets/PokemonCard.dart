ff43ba64ff6c76c69ae5443711a683b63d2d7926
Juda Alector <juda_alector@hotmail.com>
2019-07-27 19:50:00 -0500

    Arreglar problema de acomodo de componentes según el tamaño de la pantalla

--- lib/Widgets/PokemonCard.dart
+++ lib/Widgets/PokemonCard.dart
@@ -12,90 +12,88 @@ class PokemonCard extends StatelessWidget {
     Color accent = PokemonCard.chooseByPokemonType(this.pokemon.type[0]);
 
     TextStyle title = TextStyle(fontWeight: FontWeight.bold);
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-      ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
       child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: accent,
-            ),
-            child: FadeInImage.assetNetwork(
-              image: this.pokemon.thumbnailImage,
-              imageSemanticLabel: "Pokemon",
-              placeholderSemanticLabel: "Loading",
-              placeholderScale: 0.7,
-              placeholder: 'assets/load_pokeball.gif',
+          Flexible(
+            flex: 15,
+            child: Container(
+              decoration: BoxDecoration(
+                color: accent,
+              ),
+              child: FadeInImage.assetNetwork(
+                image: this.pokemon.thumbnailImage,
+                imageSemanticLabel: "Pokemon",
+                placeholderSemanticLabel: "Loading",
+                placeholderScale: 0.7,
+                placeholder: 'assets/load_pokeball.gif',
+              ),
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            margin: EdgeInsets.all(10),
-            width: width * 0.30,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  pokemon.name,
-                  style: TextStyle(
-                    color: accent,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-                Row(
-                  children: typeList(),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        "Weight",
-                        style: title,
-                      ),
-                      flex: 1,
+          Flexible(
+            flex: 13,
+            child: Container(
+              width: width * 0.30,
+              margin: EdgeInsets.only(left: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(
+                      color: accent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
                     ),
-                    Text(pokemon.weight.toString()),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        "Height",
-                        style: title,
-                      ),
-                      flex: 1,
-                    ),
-                    Text(pokemon.height.toString()),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Weakness types",
-                  style: title,
-                ),
-                Container(
-                  child: Wrap(
-                    direction: Axis.horizontal,
-                    children: weaknewssList(),
                   ),
-                )
-              ],
+                  Row(
+                    children: typeList(),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "Weight",
+                          style: title,
+                        ),
+                        flex: 1,
+                      ),
+                      Text(pokemon.weight.toString()),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "Height",
+                          style: title,
+                        ),
+                        flex: 1,
+                      ),
+                      Text(pokemon.height.toString()),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Weakness types",
+                    style: title,
+                  ),
+                  Container(
+                    child: Wrap(
+                      direction: Axis.horizontal,
+                      children: weaknewssList(),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
